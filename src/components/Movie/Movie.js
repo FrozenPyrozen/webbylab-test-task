dddb7104ba35852494465cadffaeb8154a25a3cd
Frozen <kiril.kohan@gmail.com>
2019-06-20 22:32:03 +0300

    Added confirm dialog, for movie delete

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -21,7 +21,19 @@ const Movie = ({
     <h3>{title}</h3>
     <hr />
     <footer className={styles.footer}>
-      <button onClick={onDelete} className={styles.btn_delete}>
+      <button
+        onClick={e => {
+          e.preventDefault();
+          const confirmed = window.confirm(
+            "Are you sure you wish to delete this item?"
+          );
+          if (!confirmed) {
+            return;
+          }
+          onDelete();
+        }}
+        className={styles.btn_delete}
+      >
         Delete
       </button>
       <button
